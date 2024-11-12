f4790b46ac370b849fd847d82045010799d1348f
Vaibhav <rajakumarnathnagar@gmail.com>
2024-11-12 22:18:50 +0530

    Made the app window

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton)
+
+
+class Roamify(QWidget):
+    def __init__(self):
+        super().__init__()
+
+
+#Show/Run our App
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = Roamify()
+    main_window.show()
+    sys.exit(app.exec_())
