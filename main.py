5274ea297378ff5b4739d1d68751ba8a58d3575a
Vaibhav <rajakumarnathnagar@gmail.com>
2024-11-14 05:09:39 +0530

    Basic home page layout setup

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton)
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QCalendarWidget, QVBoxLayout)
+from PyQt5.QtCore import QDate
 
 
 class Roamify(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle("Roamify")
+        self.resize(800, 600)
+
+        self.initUI()
+
+    def initUI(self):
+        # Create all App Objects
+
+        self.app_name = QLabel("Roamify")   # Will later add an image with Roamify written on it stylistically and the same bg color as that of the app
+        self.location = QLineEdit()
+        self.date_label = QLabel()
+        self.submit_btn = QPushButton("Submit")
+
+        self.calendar = QCalendarWidget()
+        self.calendar.setSelectedDate(QDate.currentDate())  # Have to add a Minimum and Maximum date range that can be selected
+        self.date_label.setText(QDate.currentDate().toString("dd-MM-yyyy"))
+
+        #All Design Here
+
+        master_layout = QVBoxLayout()
+
+        master_layout.addWidget(self.app_name)
+        master_layout.addWidget(self.location)
+        master_layout.addWidget(self.date_label)
+        master_layout.addWidget(self.calendar)
+        master_layout.addWidget(self.submit_btn)
+
+        self.setLayout(master_layout)
+
+
 
 
 #Show/Run our App
